e7e9f4fa218e766185ab9bdd934ce341034c35fb
BhargaviKolliImaginnovate <bhargavi.kolli@imaginnovate.com>
2023-06-29 09:24:47 +0530

    added

---
+++ Math.cs
@@ -0,0 +1,15 @@
+using System;
+namespace program{
+
+    class MathcSharp{
+         static void Main(string[]args){
+            int x=213,y=423;
+            Console.WriteLine(Math.Max(x,y));
+            Console.WriteLine(Math.Min(x,y));
+            Console.WriteLine(Math.Sqrt(x));
+            Console.WriteLine(Math.Abs(-4.7));
+            Console.WriteLine(Math.Round(9.99));
+
+        }
+    }
+    }
---
+++ hello.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello C-sharp!");    
+      string name="hi, i am bharu. nice to meet you";
+      Console.WriteLine(name.Contains("nice"));
+    }
+
+  }
+}
